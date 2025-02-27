5ac9f9826fe0d2f99cd8ec9e0ef8423bb528c7fd
gabriel <contato.gabrielfeitosa@gmail.com>
2025-02-27 06:53:08 -0300

    Add GridProducts component for displaying products by category

---
+++ src/components/GridProducts.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState, useContext } from "react";
+import { ProductCard } from "./ProductCard";
+import { GetContext } from "../context/getContext";
+import PropTypes from 'prop-types';
+
+export const GridProducts = ({ categoryName }) => {
+  const { products, mapCategoryName } = useContext(GetContext);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const mappedCategoryName = mapCategoryName(categoryName);
+    const filtered = products.filter(
+      (product) => product.type === mappedCategoryName
+    );
+    setFilteredProducts(filtered);
+  }, [categoryName, products, mapCategoryName]);
+
+  return (
+    <div className="col-12 col-md-9">
+      <div className="row">
+        {filteredProducts.map((product, index) => (
+          <div className="col-6 col-lg-4" key={index}>
+            <ProductCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+GridProducts.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+};
