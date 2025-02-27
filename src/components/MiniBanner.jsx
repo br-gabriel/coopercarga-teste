b928147a382070caecbc6d9ca1c9f20535ddfddc
gabriel <contato.gabrielfeitosa@gmail.com>
2025-02-27 06:52:56 -0300

    Add MiniBanner component for displaying free shipping message

---
+++ src/components/MiniBanner.jsx
@@ -0,0 +1,8 @@
+export const MiniBanner = () => {
+    return (
+        <div className="text-center py-2 border-bottom d-flex justify-content-center align-items-center gap-2">
+            <i className="bi bi-truck flip-h"></i>
+            <p className="m-0"><b>Frete grátis</b> para compras acima de R$ 350,00</p>
+        </div>
+    );
+};
