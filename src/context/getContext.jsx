fdbe3196b74c525f920418e6d736d1ca3c5005f0
gabriel <contato.gabrielfeitosa@gmail.com>
2025-02-27 06:51:14 -0300

    Add GetContext and GetContextProvider for product management

---
+++ src/context/getContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+export const GetContext = createContext();
+
+export function GetContextProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const mapCategoryName = (categoryName) => {
+    switch (categoryName) {
+      case "Camisetas":
+        return "Camiseta";
+      case "Regatas":
+        return "Regata";
+      case "Calcoes":
+        return "Calção";
+      case "Acessorios":
+        return "Acessório";
+      default:
+        return;
+    }
+  };
+
+  const formatCategoryName = (categoryName) => {
+    switch (categoryName) {
+      case "Camisetas":
+        return "Camisetas";
+      case "Regatas":
+        return "Regatas";
+      case "Calcoes":
+        return "Calções";
+      case "Acessorios":
+        return "Acessórios";
+      default:
+        return;
+    }
+  };
+
+  return (
+    <GetContext.Provider
+      value={{
+        products,
+        selectedProduct,
+        setSelectedProduct,
+        mapCategoryName,
+        formatCategoryName,
+      }}
+    >
+      {children}
+    </GetContext.Provider>
+  );
+}
+
+GetContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
